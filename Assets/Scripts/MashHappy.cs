6a6f24f969f3c5a8c917c1fa02a4db995a18c8a3
Pixsea <jiin@uci.edu>
2021-01-31 06:57:07 -0800

    Finished Basic Mashing minigame "MashHappy"
    made it so that the MinigameManager script can serve as a base class, the MashHappy uses it as a base

---
+++ Assets/Scripts/MashHappy.cs
@@ -0,0 +1,167 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MashHappy : MinigameManager
+{
+    public Text ScoreText1;  // Text to show the scor efor player 1
+    public Text ScoreText2;
+    public Text ScoreText3;
+    public Text ScoreText4;
+
+    [SerializeField]
+    private string player1Button;  // Button for player 1 to mash
+    [SerializeField]
+    private string player2Button;
+    [SerializeField]
+    private string player3Button;
+    [SerializeField]
+    private string player4Button;
+
+    private int player1Score = 0;  // How many times player 1 has mashed
+    private int player2Score = 0;  // How many times player 1 has mashed
+    private int player3Score = 0;  // How many times player 1 has mashed
+    private int player4Score = 0;  // How many times player 1 has mashed
+
+
+
+    public override void Update()
+    {
+        if (phase == "Playing")
+        {
+            if (Input.GetKeyDown(player1Button))
+            {
+                player1Score += 1;
+            }
+
+            if (Input.GetKeyDown(player2Button))
+            {
+                player2Score += 1;
+            }
+
+            if (Input.GetKeyDown(player3Button))
+            {
+                player3Score += 1;
+            }
+
+            if (Input.GetKeyDown(player4Button))
+            {
+                player4Score += 1;
+            }
+        }
+    }
+
+
+
+    public override IEnumerator GameLoop()
+    {
+        // Start off by running the 'GameStart'
+        yield return StartCoroutine(GameStarting());
+
+        // Once the 'GameStart' coroutine is finished, run the 'GamePlaying' coroutine but don't return until it's finished.
+        yield return StartCoroutine(GamePlaying());
+
+        // Once execution has returned here, run the 'GameEnd' coroutine, again don't return until it's finished.
+        yield return StartCoroutine(GameEnding());
+
+        yield return StartCoroutine(ShowScores());
+
+        yield return StartCoroutine(ShowResults());
+    }
+
+
+
+    IEnumerator ShowScores()
+    {
+        ScoreText1.text = "Player 1:\n" + player1Score.ToString();
+        ScoreText2.text = "Player 2:\n" + player2Score.ToString();
+        ScoreText3.text = "Player 3:\n" + player3Score.ToString();
+        ScoreText4.text = "Player 4:\n" + player4Score.ToString();
+
+        yield return new WaitForSeconds(3);
+    }
+
+
+
+    public override IEnumerator ShowResults()
+    {
+        ScoreText1.text = "";
+        ScoreText2.text = "";
+        ScoreText3.text = "";
+        ScoreText4.text = "";
+
+        int highest = Mathf.Max(player1Score, player2Score, player3Score, player4Score);
+
+        bool tie = false;
+        int winner = 0;
+
+        // Find the player number of the winner
+        if (player1Score == highest)
+        {
+            // If no winner has been chosen, set them as the winner
+            if (winner == 0)
+            {
+                winner = 1;
+            }
+
+            else
+            {
+                tie = true;
+            }
+        }
+
+        if (player2Score == highest)
+        {
+            // If no winner has been chosen, set them as the winner
+            if (winner == 0)
+            {
+                winner = 2;
+            }
+            // Otherwise tie
+            else
+            {
+                tie = true;
+            }
+        }
+
+        if (player3Score == highest)
+        {
+            // If no winner has been chosen, set them as the winner
+            if (winner == 0)
+            {
+                winner = 3;
+            }
+
+            else
+            {
+                tie = true;
+            }
+        }
+
+        if (player4Score == highest)
+        {
+            // If no winner has been chosen, set them as the winner
+            if (winner == 0)
+            {
+                winner = 4;
+            }
+
+            else
+            {
+                tie = true;
+            }
+        }
+
+        if (tie)
+        {
+            UIMainText.text = "TIE";
+        }
+        else
+        {
+            UIMainText.text = "Player " + winner.ToString() + " Wins!";
+        }
+
+        yield return resultsWait;
+    }
+}
--- Assets/Scripts/MinigameManager.cs
+++ Assets/Scripts/MinigameManager.cs
@@ -6,69 +6,76 @@ using UnityEngine.UI;
 public class MinigameManager : MonoBehaviour
 {
     public GameObject[] Players;
-    private string phase;  // Current phase
-    private bool gameEnded = false;  // Is false while the game is running, can be turned true by calling EndGame
+    [HideInInspector]
+    public string phase;  // Current phase
+    [HideInInspector]
+    public bool gameEnded = false;  // Is false while the game is running, can be turned true by calling EndGame
     public Text UIMainText;  // Reference to the UI Text to display winning text, etc.
     public Text UITimerText;  // Reference to the UI Text to display the timer
 
 
     [SerializeField]
-    private bool UsesTimer = true; // Whether the game uses a timer
+    public bool UsesTimer = true; // Whether the game uses a timer
     [SerializeField]
-    private int TotalGameTime;  // the start time in seconds for the minigame
-    private float timer;  // Current timer for game in seconds
+    public int TotalGameTime;  // the start time in seconds for the minigame
+    [HideInInspector]
+    public float timer;  // Current timer for game in seconds
 
     [SerializeField]
-    private float startWaitSec;  // How long the start of game phase lasts before it control is given
+    public float startWaitSec;  // How long the start of game phase lasts before it control is given
     [SerializeField]
-    private float endWaitSec;  // How long the end of game phase lasts before it scene is switched
+    public float endWaitSec;  // How long the end of game phase lasts before it scene is switched
+    [SerializeField]
+    public float resultsWaitSec;  // How long the end of game phase lasts before it scene is switched
 
-    private WaitForSeconds startWait;  // Used to have a delay whilst the round starts.
-    private WaitForSeconds endWait;  // Used for delay on round end
+    [HideInInspector]
+    public WaitForSeconds startWait;  // Used to have a delay whilst the round starts.
+    [HideInInspector]
+    public WaitForSeconds resultsWait;  // Used for delay when results how
 
 
 
 
     // Start is called before the first frame update
-    void Start()
+    public virtual void Start()
     {
         // Since we decrease timer by 1 every FixedUpdate, set the timer to the TotalGameTime
         // divided by Time.fixedDeltaTime (a decimal of the delay between frames of FixedUpdateCalls)
         timer = TotalGameTime / Time.fixedDeltaTime;
 
         startWait = new WaitForSeconds(startWaitSec);
-        endWait = new WaitForSeconds(endWaitSec);
+        resultsWait = new WaitForSeconds(resultsWaitSec);
 
         phase = "Start";
         StartCoroutine(GameLoop());
     }
 
     // Update is called once per frame
-    void Update()
+    public virtual void Update()
     {
-
     }
 
-    void FixedUpdate()
+
+
+    public virtual void FixedUpdate()
     {
         //Debug.Log(timer);
-        if (phase == "Playing")
+        if ((phase == "Playing" || phase == "End") && UsesTimer)
         {
             timer--;
         }
     }
 
-    void Startgame()
-    {
 
-    }
 
-    void EndGame()
+    public virtual void EndGame()
     {
         gameEnded = true;
     }
 
-    private IEnumerator GameLoop()
+
+
+    public virtual IEnumerator GameLoop()
     {
         // Start off by running the 'GameStart'
         yield return StartCoroutine(GameStarting());
@@ -79,10 +86,12 @@ public class MinigameManager : MonoBehaviour
         // Once execution has returned here, run the 'GameEnd' coroutine, again don't return until it's finished.
         yield return StartCoroutine(GameEnding());
 
-        UIMainText.text = "";
+        yield return StartCoroutine(ShowResults());
     }
 
-    private IEnumerator GameStarting()
+
+
+    public virtual IEnumerator GameStarting()
     {
         UIMainText.text = "Ready?";
         UITimerText.text = "";
@@ -91,13 +100,17 @@ public class MinigameManager : MonoBehaviour
         yield return startWait;
     }
 
-    private IEnumerator GamePlaying()
+
+
+    public virtual IEnumerator GamePlaying()
     {
         phase = "Playing";
         UIMainText.text = "GO!";
 
+        timer = TotalGameTime / Time.fixedDeltaTime;
+
         // While the game hasn't ended and while the timer is greater than 0
-        while(!gameEnded && (timer > 0))
+        while (!gameEnded && (timer > 0))
         {
             // If more than 1.5 seconds has past, remove "GO!" from the UI
             if (((TotalGameTime / Time.fixedDeltaTime) - timer) > (1.5 / Time.fixedDeltaTime))
@@ -105,7 +118,10 @@ public class MinigameManager : MonoBehaviour
                 UIMainText.text = "";
             }
 
-            UITimerText.text = (Mathf.Ceil(timer * Time.fixedDeltaTime)).ToString();
+            if (UsesTimer)
+            {
+                UITimerText.text = (Mathf.Ceil(timer * Time.fixedDeltaTime)).ToString();
+            }
 
             // ... return on the next frame.
             yield return null;
@@ -113,12 +129,32 @@ public class MinigameManager : MonoBehaviour
     }
 
 
-    private IEnumerator GameEnding()
+
+    public virtual IEnumerator GameEnding()
     {
         phase = "End";
         UIMainText.text = "Finish";
+        UITimerText.text = "";
 
-        // Wait for the specified length of time until yielding control back to the game loop.
-        yield return endWait;
+        timer = endWaitSec / Time.fixedDeltaTime;
+
+        while(timer > 0)
+        {
+            // If more than 1.5 seconds has past, remove "Finish!" from the UI
+            if (((endWaitSec / Time.fixedDeltaTime) - timer) > (1.5 / Time.fixedDeltaTime))
+            {
+                UIMainText.text = "";
+            }
+
+            yield return null;
+        }
+    }
+
+
+
+    public virtual IEnumerator ShowResults()
+    {
+        UIMainText.text = "Someone won hopefully";
+        yield return resultsWait;
     }
 }
