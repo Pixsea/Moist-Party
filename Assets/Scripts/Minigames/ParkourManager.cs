991a0e348bc9cfa3547fd66bba6bc084ce92f458
Alexander Le <alexanvl@uci.edu>
2021-03-17 21:58:34 -0700

    asdf

--- Assets/Scripts/Minigames/ParkourManager.cs
+++ Assets/Scripts/Minigames/ParkourManager.cs
@@ -7,7 +7,7 @@ public class ParkourManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //asdf
     }
 
     // Update is called once per frame
