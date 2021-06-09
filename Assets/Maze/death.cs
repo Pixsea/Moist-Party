35069314955fd342dcd19afbfd38ea832b1b5651
PolyPanda <747173835+PolyPanda@users.noreply.github.com>
2021-06-09 16:58:13 -0700

    Maze
    Maze mini game added

---
+++ Assets/Maze/death.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class death : MonoBehaviour
+{
+    Vector3 startPos;
+
+    private void Awake()
+    {
+        startPos = this.transform.position;
+    }
+
+    //void OnCollisionEnter(Collision other)
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("spike"))
+        {
+            Respawn();
+        }
+            
+    }
+    
+    void Respawn()
+    {
+        this.transform.position = startPos;
+    }
+
+}
