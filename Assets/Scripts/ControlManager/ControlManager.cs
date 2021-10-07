f21c4d1a809e19bef847ae09e18423cda8defec6
DanyeWestinator <44176712+DanyeWestinator@users.noreply.github.com>
2021-10-06 23:22:47 -0700

    work on multi-controller support

--- Assets/Scripts/ControlManager/ControlManager.cs
+++ Assets/Scripts/ControlManager/ControlManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 namespace MultiInput
 {
@@ -16,37 +17,57 @@ namespace MultiInput
         /// The HashSet of all keys that were down that frame
         /// </summary>
         public HashSet<KeyCode> Keys { get; private set; } = new HashSet<KeyCode>();
+        /// <summary>
+        /// The list of held down keys, for debug purposes
+        /// </summary>
+        [SerializeField]
+        public List<KeyCode> downList = new List<KeyCode>();
+
+        public bool joystickHeld;
+        public float horizontalAxis;
+        public float verticalAxis;
 
         // Update is called once per frame
         void Update()
         {
-            string held = "";
-            ButtonsDown();
-            if (string.IsNullOrEmpty(held) == false)
-                print(held);
+            GetButtonsPressed();
+            downList = Keys.ToList();
+            horizontalAxis = Input.GetAxis("HorizontalTest");
+            verticalAxis = Input.GetAxis("VerticalTest");
+            joystickHeld = (Input.GetAxis("HorizontalTest") != 0);
         }
         
         /// <summary>
-        /// Gets the list of KeyCodes for all currently held down keys
+        /// Updates the lists of both GetKey and GetKeyDown that frame
         /// </summary>
-        /// <returns>Returns a list of KeyCodes for all buttons that started being held down that frame</returns>
-        private void ButtonsDown()
+        private void GetButtonsPressed()
         {
             //iterate through every key
             foreach (KeyCode key in Enum.GetValues(typeof(KeyCode)))
             {
-                //If the key is held down and not currently in the list
+                //If the key is was first held down this frame and not currently in the list
                 if (Input.GetKeyDown(key) && KeysDown.Contains(key) == false)
                 {
                     KeysDown.Add(key);
                 }
                 //Remove from the list if not held down and in the list
-                else if (KeysDown.Contains(key))
+                else if (KeysDown.Contains(key) && Input.GetKeyDown(key))
                 {
                     KeysDown.Remove(key);
                 }
+                //If the key is held down and not currently in the list
+                if (Input.GetKey(key) && Keys.Contains(key) == false)
+                {
+                    Keys.Add(key);
+                }
+                //Remove from the list if not held down and in the list
+                else if (Keys.Contains(key) && Input.GetKey(key) == false)
+                {
+                    Keys.Remove(key);
+                }
             }
         }
+        
     }
 
 }
